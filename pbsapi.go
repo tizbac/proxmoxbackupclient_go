d130a814e86bd8ebd26f4e5a627f9ab19e691630
lookup82 <luca.crimella@gmail.com>
2024-07-04 16:22:58 +0200

    hostname moved into if

--- pbsapi.go
+++ pbsapi.go
@@ -348,8 +348,8 @@ func (pbs *PBSClient) Connect(reader bool) {
 
 	pbs.manifest.BackupTime = time.Now().Unix()
 	pbs.manifest.BackupType = "host"
-	hostname, _ := os.Hostname()
 	if pbs.manifest.BackupID == "" {
+		hostname, _ := os.Hostname()
 		pbs.manifest.BackupID = hostname
 	}
 	pbs.client = http.Client{
